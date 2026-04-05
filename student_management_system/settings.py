4582af3c442a3def09f41d93d3accea868be2c01
ayesha-devx <ayesha111206@gmail.com>
2026-04-06 00:01:35 +0530

    Final static storage fix for stable Render build

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -142,7 +142,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "main_app/static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 AUTH_USER_MODEL = 'main_app.CustomUser'
 AUTHENTICATION_BACKENDS = ['main_app.EmailBackend.EmailBackend']
